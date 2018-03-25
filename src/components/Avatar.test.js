52f2ac9a77f31c26bb51ddeaa4d6ada1a7d4871b
Victor Tiunov <tiunoff@gmail.com>
2018-03-25 21:32:54 +0500

    Added first snapshot test

---
+++ src/components/Avatar.test.js
@@ -0,0 +1,19 @@
+/* eslint-env jest */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Avatar from './Avatar';
+
+describe('<Avatar />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Avatar colorFrom="Harry Potter">Harry Potter</Avatar>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<Avatar colorFrom="Harry Potter">Harry Potter</Avatar>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});

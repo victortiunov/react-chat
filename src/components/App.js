2e3adce2d4c0a184845ee66651d337bfc5212d28
Victor Tiunov <tiunoff@gmail.com>
2018-03-25 20:35:44 +0500

    Added first Jest test

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
-import { Provider } from 'react-redux';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRoute from '../containers/PrivateRoute';
 import ChatPage from '../containers/ChatPage';
 import WelcomePage from '../containers/WelcomePage';
 import history from '../utils/history';
-import configureStore from '../store';
 
 const styles = theme => ({
   root: {
@@ -18,18 +16,14 @@ const styles = theme => ({
   },
 });
 
-const store = configureStore();
-
 const App = () => (
-  <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" component={WelcomePage} />
-        <PrivateRoute path="/chat" component={ChatPage} />
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  </Provider>
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={WelcomePage} />
+      <PrivateRoute path="/chat" component={ChatPage} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
 );
 
 export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store';
 import './index.css';
 
-const rootElement = document.getElementById('root');
+const rootEl = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+const store = configureStore();
+
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    rootEl,
+  );
+};
+
+render(App);
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    ReactDOM.render(<App />, rootElement);
+    render(App);
   });
 }
 
